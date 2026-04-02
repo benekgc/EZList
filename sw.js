3eefdab2260c7b39516e333859dd56de6aae87f6
benekgc <benjamin.buyukozturk@student.wsb.edu.pl>
2026-04-02 23:32:51 +0200

    PWA update

--- sw.js
+++ sw.js
@@ -1,16 +1,13 @@
-// Jak kiedyś zaktualizujesz apkę, zmień to np. na 'ezlist-cache-v2'
-const CACHE_NAME = 'ezlist-cache-v1';
+const CACHE_NAME = 'ezlist-cache-v2';
 
-// Lista plików, które chcemy pobrać i zapisać offline
 const FILES_TO_CACHE = [
-  './',             // Główny katalog (bardzo ważne!)
-  './index.html',   // Twój kod
-  './manifest.json',// Twój manifest
-  './icon-192.png', // Twoje ikony
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
   './icon-512.png'
 ];
 
-// 1. Etap instalacji: pobieramy pliki i wrzucamy do Cache
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -18,25 +15,20 @@ self.addEventListener('install', (event) => {
       return cache.addAll(FILES_TO_CACHE);
     })
   );
-  // Zmusza nowy Service Worker do natychmiastowego przejęcia kontroli
   self.skipWaiting(); 
 });
 
-// 2. Etap działania: przechwytujemy zapytania z apki (tzw. Cache First)
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      // Jeśli plik jest w pamięci telefonu, zwracamy go natychmiast (działa offline!)
       if (response) {
         return response;
       }
-      // Jeśli go nie ma (np. jakiś link do zewnętrznej strony), próbujemy pobrać z sieci
       return fetch(event.request);
     })
   );
 });
 
-// 3. Etap aktywacji: sprzątanie starych wersji
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keyList) => {
